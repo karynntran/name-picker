12f0b8b250c16177a4af645904c21963be6cd66d
karynntran <karynn.tran@gmail.com>
2015-03-28 18:27:14 -0400

    Add names from SSA database

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,38 +1,38 @@
-var boyNames = ["chase", "benjamin", "nicholas"]
+
 
 function randomizeNames(gender){
-	return gender[Math.floor(Math.random()*gender.length)];
+	return gender[Math.floor(Math.random()*gender.length)].name;
 }
 
 $( document ).ready (function(){
 	$('#first').submit(function( e ) {
 		e.preventDefault();
 		name = $(this).children("input[name]").val();
-		$( '#first-name').html(name);
+		$( '#first-name').html(name.toUpperCase());
 	});
 
 	$('#middle').submit(function( e ) {
 		e.preventDefault();
 		name = $(this).children("input[name]").val();
-		$( '#middle-name').html(name);
+		$( '#middle-name').html(name.toUpperCase());
 	});
 
 	$('#last').submit(function( e ) {
 		e.preventDefault();
 		name = $(this).children("input[name]").val();
-		$( '#last-name').html(name);
+		$( '#last-name').html(name.toUpperCase());
 	});
 
 	$( '#randomize-first' ).click(function(e) {
 	  e.preventDefault();
 	  name = randomizeNames(boyNames);
-	  $( '#first-name').html(name);
+	  $( '#first-name').html(name.toUpperCase());
 	});
 
 	$( '#randomize-middle' ).click(function(e) {
 	  e.preventDefault();
 	  name = randomizeNames(boyNames);
-	  $( '#middle-name').html(name);
+	  $( '#middle-name').html(name.toUpperCase());
 	});
 
 
