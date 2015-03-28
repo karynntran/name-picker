67a206dc7eb014ec071dd8c000ca2767f4d361a0
karynntran <karynn.tran@gmail.com>
2015-03-28 17:41:57 -0400

    Add input buttons for all names

--- javascripts/main.js
+++ javascripts/main.js
@@ -6,4 +6,17 @@ $( document ).ready (function(){
 		name = $(this).children("input[name]").val();
 		$( '#first-name').html(name);
 	});
+
+	$('#middle').submit(function( e ) {
+		e.preventDefault();
+		name = $(this).children("input[name]").val();
+		$( '#middle-name').html(name);
+	});
+
+	$('#last').submit(function( e ) {
+		e.preventDefault();
+		name = $(this).children("input[name]").val();
+		$( '#last-name').html(name);
+	});
+
 })
