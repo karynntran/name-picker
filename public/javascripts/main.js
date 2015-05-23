0aac914a00064563341213437355f7554d4a6308
karynntran <karynn.tran@gmail.com>
2015-05-23 09:59:30 +0900

    Refactor submit button functionality using closures'

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -11,37 +11,72 @@ function randomizeNames(gender){
 	return gender[Math.floor(Math.random()*gender.length)].name;
 }
 
-$( document ).ready (function(){
-	$('#first').submit(function( e ) {
-		e.preventDefault();
-		name = $(this).children("input[name]").val();
-		$( '#first-name').html(name.toUpperCase());
-	});
 
-	$('#middle').submit(function( e ) {
-		e.preventDefault();
-		name = $(this).children("input[name]").val();
-		$( '#middle-name').html(name.toUpperCase());
-	});
+//on window onload
+document.addEventListener('DOMContentLoaded', function(){
 
-	$('#last').submit(function( e ) {
-		e.preventDefault();
-		name = $(this).children("input[name]").val();
-		$( '#last-name').html(name.toUpperCase());
-	});
+	var nameTypeArray = ["first", "middle", "last"];
 
-	$( '#randomize-first' ).click(function(e) {
-	  e.preventDefault();
-	  name = randomizeNames(boyNames);
-	  $( '#first-name').html(name.toUpperCase());
-	});
+	for (var i = 0; i < nameTypeArray.length; i++){
+		var submitButton = document.getElementById(nameTypeArray[i] + "Submit");
+		var submitResult = document.getElementById(nameTypeArray[i] + "Name");
 
-	$( '#randomize-middle' ).click(function(e) {
-	  e.preventDefault();
-	  name = randomizeNames(boyNames);
-	  $( '#middle-name').html(name.toUpperCase());
-	});
+		submitButton.onclick = (function(submitResult) {
+            return function(event) {
+                event.preventDefault();
+                var name = this.previousElementSibling.value;
+                submitResult.textContent = name;
+            }
+        })(submitResult);
+	};
 
-	chooseGender();
 
-})
+	// $( '#randomize-first' ).click(function(e) {
+	//   e.preventDefault();
+	//   name = randomizeNames(boyNames);
+	//   $( '#first-name').html(name.toUpperCase());
+	// });
+
+	// $( '#randomize-middle' ).click(function(e) {
+	//   e.preventDefault();
+	//   name = randomizeNames(boyNames);
+	//   $( '#middle-name').html(name.toUpperCase());
+	// });
+
+	// chooseGender();
+});
+
+// $( document ).ready (function(){
+// 	$('#first').submit(function( e ) {
+// 		e.preventDefault();
+// 		name = $(this).children("input[name]").val();
+// 		$( '#first-name').html(name.toUpperCase());
+// 	});
+
+// 	$('#middle').submit(function( e ) {
+// 		e.preventDefault();
+// 		name = $(this).children("input[name]").val();
+// 		$( '#middle-name').html(name.toUpperCase());
+// 	});
+
+// 	$('#last').submit(function( e ) {
+// 		e.preventDefault();
+// 		name = $(this).children("input[name]").val();
+// 		$( '#last-name').html(name.toUpperCase());
+// 	});
+
+// 	$( '#randomize-first' ).click(function(e) {
+// 	  e.preventDefault();
+// 	  name = randomizeNames(boyNames);
+// 	  $( '#first-name').html(name.toUpperCase());
+// 	});
+
+// 	$( '#randomize-middle' ).click(function(e) {
+// 	  e.preventDefault();
+// 	  name = randomizeNames(boyNames);
+// 	  $( '#middle-name').html(name.toUpperCase());
+// 	});
+
+// 	chooseGender();
+
+// })
